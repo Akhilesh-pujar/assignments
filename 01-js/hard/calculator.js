dc5c2eaaf410eb0c4dfe9a5bdb598718f56c5e31
Akhilesh <akhileshpujar796@gmail.com>
2023-12-13 10:50:03 +0530

    six-testcaseremainsincalculator

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,51 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(number) {
+    this.result += number;
+  }
+
+  subtract(number) {
+    this.result -= number;
+  }
+
+  multiply(number) {
+    this.result *= number;
+  }
+
+  divide(number) {
+    if (number === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    this.result /= number;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+    // Remove multiple continuous spaces and trim the expression
+    const cleanedExpression = expression.replace(/\s+/g, ' ').trim();
+
+    if (!/^\s*-?\d+(\.\d+)?\s*([\+\-\*\/]\s*-?\d+(\.\d+)?\s*)*$/g.test(cleanedExpression)) {
+      throw new Error("Invalid expression: contains non-numerical characters");
+    }
+
+    this.result = eval(cleanedExpression);
+  }
+}
+
+
+
 
 module.exports = Calculator;
