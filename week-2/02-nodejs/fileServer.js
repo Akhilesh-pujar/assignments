22d89ccf586fc60792b6f861635e612c62f1ac17
Akhilesh <akhileshpujar796@gmail.com>
2023-12-16 10:52:35 +0530

    nodejsfileserverdone

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -16,6 +16,31 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+app.get("/files", (req,res)=>{
 
+ fs.readdir(path.join(__dirname, './files/'),(err,files)=>{
+  if(err){
+    return res.statusCode(500).json({error:'Failed to retrieve files'})
+  }
+  res.json(files);
+ })
+  return res;
+
+})
+
+app.get("/file/:filename",(req,res)=>{
+  const filespath = path.join(__dirname,'./files/', req.params.filename);
+
+  fs.readFile(filespath,"utf-8",(err,data)=>{
+    if(err){
+      return res.status(404).send('File not found')
+    }
+    res.send(data)
+  })
+
+})
+app.all('*', (req, res) => {
+  res.status(404).send('Route not found');
+});
 
 module.exports = app;
