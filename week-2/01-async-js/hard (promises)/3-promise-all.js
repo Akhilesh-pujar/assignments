a555ad693831abbb5fe4d3bb772d0035f4528764
Akhilesh <akhileshpujar796@gmail.com>
2023-12-16 08:29:41 +0530

    promise-all-done

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,18 +5,46 @@
  */
 
 function wait1(t) {
+    return new Promise(function(resolve){
+        setTimeout(function(){
+          resolve()
+        },t*1000)
+
+    })
+   
 
 }
 
 function wait2(t) {
+    return new Promise(function(resolve){
+        setTimeout(function(){
+          resolve()
+        },t*1000)
+
+    })
+   
 
 }
 
 function wait3(t) {
+    return new Promise(function(resolve){
+        setTimeout(function(){
+          resolve()
+        },t*1000)
 
+    })
 }
 
+
 function calculateTime(t1, t2, t3) {
+    const starttime = Date.now();
+     let res = Promise.all([wait1(t1), wait2(t2), wait3(t3)])
+     return res.then(()=>{
+        const endtime = Date.now()
+        return endtime - starttime
+
+     })
+
 
 }
 
